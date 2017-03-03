287093ceec38e4368414e322f5c05fe18b9f4114
ErickaZC <cuchazc96@gmail.com>
2017-03-02 19:10:53 -0600

    Arreglos pequenos

--- Laboratorio03/GraphWithPointer/GraphWithPointer.h
+++ Laboratorio03/GraphWithPointer/GraphWithPointer.h
@@ -16,12 +16,12 @@ class GraphWithPointer {
         int numNodes;
         //! Atributo que indica la maxima cantidad de elementos que permite graph.
         int maxElements;
-        
+
         /*! Constructor de la clase grafo.*/
         GraphWithPointer(){
             graph = nullptr;
         }
-        
+
         /*! addEdge, se agrega una arista a conexions del nodo s.
          *\param w peso
          *\param s source Node
@@ -31,20 +31,20 @@ class GraphWithPointer {
         {
             s->addEdge(Edge<D>(w,s,d));
         }
-        
+
         /*! addNode Agrega un nodo a graph
          \param d dato que contendrá el nodo agregado.*/
         Node<D>* addNode(D* d)
         {
             Node<D>* newNode = new Node<D>(d);
-            if (numNodes == 0) 
+            if (numNodes == 0)
             {
                 this->graph = new Node<D>*[10];
                 this->numNodes = 1;
                 maxElements = 10;
                 this->graph[0] = newNode;
             } else { //list con algo
-                if (numNodes+1 != maxElements) { 
+                if (numNodes+1 != maxElements) {
                     this->graph[numNodes] = newNode;
                     this->numNodes++;
                 } else { //arreglo sin espacio
@@ -61,7 +61,7 @@ class GraphWithPointer {
             }
             return newNode;
         }
-        
+
         /*! Elimina una arista de la lista de conexiones del nodo s.
          \param s source node
          \param d destiny node*/
@@ -69,7 +69,7 @@ class GraphWithPointer {
         {
             s->delEdge(s,d);
         }
-        
+
         /*! Elimina el nodo que contiene al dato d de la lista graph.
          * De manera que también elimina todas las aristas que lleguen a dicho nodo.
          * \param d dato que contiene el nodo que se desea borrar. */
@@ -77,7 +77,7 @@ class GraphWithPointer {
         {
             int pos=-1;
             Node<D>* a = dfs(d);
-            
+
             for(int j=0; j<numNodes;j++)
             {
                 if(!(*(graph[j]->data)==*(a->data)))
@@ -89,8 +89,8 @@ class GraphWithPointer {
                     pos = j;
                 }
             }
-            
-            
+
+
             if(pos >0)
             {
                 for(int i=pos; i<numNodes-1;i++){
@@ -100,7 +100,7 @@ class GraphWithPointer {
                 numNodes--;
             }
         }
-        
+
         /*! Indica el primer nodo que contiene graph.
          \return el primer nodo.*/
         Node<D>* firstNode()
@@ -111,7 +111,7 @@ class GraphWithPointer {
             }
             else return nullptr;
         }
-        
+
         /*! Indica el nodo que le sigue al nodo t.
          \param t nodo
          \return el nodo siguiente a t*/
@@ -125,7 +125,7 @@ class GraphWithPointer {
                 }
             }
         }
-        
+
         /*! Indica el nodo que se encuentra en la posición i de graph
          \param i posicion
          \return nodo que se encuentra en la posicion i de graph*/
@@ -133,7 +133,7 @@ class GraphWithPointer {
         {
             return graph[i];
         }
-        
+
         /*! Indica el dato que contiene un nodo
          \param a Nodo
          \return dato que contiene a*/
@@ -141,11 +141,11 @@ class GraphWithPointer {
         {
             return a->data;
         }
-        
+
         /*! Depth first search.
-         *  Recorre el grafo, desde su raíz y avanza de vértice en vértice, marcando cada vértice como visitado. 
+         *  Recorre el grafo, desde su raíz y avanza de vértice en vértice, marcando cada vértice como visitado.
          * La búsqueda siempre avanza hacia un vértice no marcado,internándose “profundamente” en el grafo
-         * sin repetir ningún vértice. Cuando se alcanza un vértice cuyos vecinos han sido marcados, 
+         * sin repetir ningún vértice. Cuando se alcanza un vértice cuyos vecinos han sido marcados,
          * se retrocede al anterior vértice visitado y se avanza desde éste.
          \param d dato buscado
          \return El nodo dentro del grafo donde se ubica el dato.
@@ -156,7 +156,7 @@ class GraphWithPointer {
             {
                 graph[i]->beenHere = false;
             }
-            
+
             int nodeCounter=0;
             Stack<Node<D>,Cell<Node<D>>*>* q1 = new Stack<Node<D>,Cell<Node<D>>*>();
             int i=0;
@@ -175,30 +175,30 @@ class GraphWithPointer {
                         }
                         int beenHereCount = 0;
                         if(temp->numconexions != 0){
-                            
+
                             Node<D>* neighbor = temp->conexions[0].successor;
                             bool  limit = false;
                             while(neighbor->beenHere && !limit)
                             {
                                 beenHereCount++;
-                                if (beenHereCount != temp->numconexions) 
+                                if (beenHereCount != temp->numconexions)
                                 {
                                     neighbor = temp->conexions[beenHereCount].successor;
                                 }
-                                else 
+                                else
                                 {
                                     limit = true;
                                 }
                             }
-                            
-                            if(beenHereCount == temp->numconexions) 
+
+                            if(beenHereCount == temp->numconexions)
                             {
                                 q1->pop();
                                 if(q1->first){
                                     temp=q1->last->data;
                                 }
                             }
-                            else 
+                            else
                             {
                                 temp = neighbor;
                                 q1->insert(temp);
@@ -214,18 +214,18 @@ class GraphWithPointer {
                             }
                         }
                     }
-                    
-                    
+
+
                     i++;
                 }
                 else i++;
             }
             cout<<"No se encontro el valor indicado"<<endl;
-            return nullptr;  
+            return nullptr;
         }
-        
+
         /*! Breadth first search
-         *La busqueda se comienza desde la raíz que es el primer vértice activo. 
+         *La busqueda se comienza desde la raíz que es el primer vértice activo.
          * En el siguiente paso se etiquetan como visitados todos los vecinos del
          *vértice activo que no han sido etiquetados. Se continúa etiquetando todos los vecinos de
          *los hijos de v que no hayan sido visitados aún.
@@ -279,13 +279,13 @@ class GraphWithPointer {
                 if(nodeCounter==numNodes)
                 {
                     cout<<"No se encontro el valor indicado"<<endl;
-                    return nullptr;  
+                    return nullptr;
                 }
             }
-            
-            
+
+
         }
-        
+
         /*! Indica la posicion dentro de graph donde se ubica el nodo s
          \param s nodo
          * \return posicion
@@ -300,39 +300,39 @@ class GraphWithPointer {
                 }
             }
         }
-        
+
         /*! Indica el peso correspondiente de ir de un nodo s, a cada uno de los nodos dentro del grafo
          * \param s source Node
          * \return arreglo de tipo double. */
         double* dijkstra(Node<D>* s)
         {
             double* route = new double[numNodes];
-            
+
             bool found = false;
             int pos = 0;
             while(!found){
                 if(graph[pos] != s) {
                     pos++;
                 }
-                else 
+                else
                 {
                     found = true;
                 }
             }
-            
+
             for(int i=0; i < numNodes; i++)
             {
                 graph[i]->beenTemp=false;
                 route[i] = INT_MAX;
             }
-            
+
             route[pos] = 0;
-            
+
             Node<D>* temp = graph[pos];
             Node<D>* temp3;
             double minWeight =0;
             double preMinWeight=0;
-            
+
             while(minWeight != INT_MAX){
                 for(int k=0;k<numNodes;k++)
                 {
@@ -354,21 +354,21 @@ class GraphWithPointer {
                         }
                     }
                 }
-                
+
                 double minConex = INT_MAX;
                 temp->beenTemp = true;
                 for(int i=0; i<temp->numconexions;i++)
                 {
-                    
-                    
+
+
                     Node<D>* neighbor = temp->conexions[i].successor;
                     double nWeight = temp->conexions[i].weight;
                     double actualWeight = nWeight+minWeight;
-                    if (route[index(neighbor)] > actualWeight) 
+                    if (route[index(neighbor)] > actualWeight)
                     {
-                        
+
                         route[index(neighbor)] = actualWeight;
-                        if (actualWeight < minConex && neighbor->numconexions!=0 && !(neighbor->beenTemp)) 
+                        if (actualWeight < minConex && neighbor->numconexions!=0 && !(neighbor->beenTemp))
                         {
                             minConex = actualWeight;
                             temp3 = neighbor;
@@ -379,7 +379,7 @@ class GraphWithPointer {
                 minWeight = minConex;
                 temp=temp3;
             }
-            
+
             for(int i=0;i<numNodes;i++)
             {
                 cout<<route[i]<<",";
@@ -387,7 +387,7 @@ class GraphWithPointer {
             cout<<endl;
             return route;
         }
-        
+
         /*! Indica el peso que tiene de ir de cada uno de los nodos de graph, a los otros nodos pertenecientes a graph
          \return arreglo en 2D de doubles.*/
         double** floyd()
@@ -397,9 +397,9 @@ class GraphWithPointer {
             {
                 routes[i]=dijkstra(graph[i]);
             }
-            
+
         }
-        
+
         /*! Destructor de la clase GraphWithPointer*/
         virtual ~GraphWithPointer(){
             for (int i=0; i<numNodes;i++)
@@ -412,4 +412,3 @@ class GraphWithPointer {
 };
 
 #endif /* GRAPHWITHPOINTER_H */
-
--- Laboratorio03/GraphWithPointer/main.cpp
+++ Laboratorio03/GraphWithPointer/main.cpp
@@ -17,8 +17,8 @@ int main(int argc, char** argv) {
     Node<MyType>* nF = g1->addNode(new MyType('f'));
     Node<MyType>* nG = g1->addNode(new MyType('g'));
     Node<MyType>* nH = g1->addNode(new MyType('h'));
-    
-    
+
+
     g1->addEdge(20,nA,nB);
     g1->addEdge(80,nA,nD);
     g1->addEdge(90,nA,nG);
@@ -28,38 +28,83 @@ int main(int argc, char** argv) {
     g1->addEdge(10,nF,nC);
     g1->addEdge(50,nC,nF);
     g1->addEdge(10,nC,nD);
-    g1->addEdge(20,nC,nH);    
+    g1->addEdge(20,nC,nH);
     g1->addEdge(10,nD,nC);
     g1->addEdge(20,nD,nG);
     g1->addEdge(50,nE,nB);
     g1->addEdge(30,nE,nG);
- 
-    double* route1 = g1->dijkstra(g1->dfs(new MyType('c')));
-    double* route2 = g1->dijkstra(g1->bfs(new MyType('b')));
+
+    cout<<"Nodos que contiene el grafo"<<endl;
+    cout<<"[";
+    for(int i=0;i<g1->numNodes;i++)
+    {
+        if(i!=g1->numNodes-1)
+        {
+          cout<<**(g1->graph[i]->data)<<",";
+        }
+        else
+        {
+          cout<<**(g1->graph[i]->data)<<"]"<<endl;
+        }
+    }
+    cout<<endl;
+    cout<<endl;
+
+    cout<< "Aristas que contiene el Nodo d:"<<endl;
+
+    cout<<"[";
+    for(int i=0;i<nD->numconexions;i++)
+    {
+      char s = nD->conexions[i].predecessor->data->content;
+      char d = nD->conexions[i].successor->data->content;
+      int w = nD->conexions[i].weight;
+      if(i!=nD->numconexions-1){
+        cout<< "("<<s<<" to "<<d<<":"<<w<<"),";
+      }
+      else
+      {
+        cout<< "("<<s<<" to "<<d<<":"<<w<<")]"<<endl;
+      }
+    }
+    cout<<endl;
+    cout<<endl;
+
+    cout<<"Pesos para llegar de c a cada uno de los otros nodos:"<<endl;
+    double* route1 = g1->dijkstra(g1->bfs(new MyType('c')));
     delete [] route1;
-    delete [] route2;
-    
     cout<<endl;
     cout<<endl;
-    
+
+    cout<<"Pesos para llegar de cada uno de los nodos dentro del grafo a los otros nodos:"<<endl;
     double** routes = g1->floyd ();
-    
-    for(int i=0;i<g1->numNodes;i++)
-    {
-        cout<<**(g1->graph[i]->data)<<",";
-    }
-    
-    g1->delNode(new MyType('c'));
-    
-    for(int i=0;i<g1->numNodes;i++)
-    {
-        cout<<**(g1->graph[i]->data)<<",";
-    }
-    
     cout<<endl;
-    
+    cout<<endl;
+
+    cout<<"Se elimina el nodo c"<<endl;
+    g1->delNode(new MyType('c'));
+    cout<<endl;
+    cout<<endl;
+
+    cout<<"Nodos que contiene el grafo"<<endl;
+    cout<<"[";
+    for(int i=0;i<g1->numNodes;i++)
+    {
+        if(i!=g1->numNodes-1)
+        {
+          cout<<**(g1->graph[i]->data)<<",";
+        }
+        else
+        {
+          cout<<**(g1->graph[i]->data)<<"]"<<endl;
+        }
+    }
+    cout<<endl;
+    cout<<endl;
+
+    cout<<"Busqueda mediante dfs de c"<<endl;
     g1->dfs(new MyType('c'));
-    g1->bfs(new MyType('c'));
+    cout<<endl;
+    cout<<endl;
+
     return 0;
 }
-
